332808b848834cfffb4e4f26e73766f3f8d80c49
pedrofixe <yurbypedro@hotmai.com>
2018-10-03 14:27:17 +0100

    Finished sphere

--- 1/Sphere.js
+++ 1/Sphere.js
@@ -31,7 +31,6 @@ class Sphere extends CGFobject
 		{
 			for (var i = 0; i < this.slices; i++)
 			{
-
 				this.vertices.push(Math.cos(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)), Math.sin(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)), j/this.stacks);				// A
 
 				if (j == this.stacks -1)
@@ -52,39 +51,44 @@ class Sphere extends CGFobject
 
 		}
 
-		// this.vertices.push(0,0,1); // Center
-		// this.normals.push(0,0,1); // Center
-        // this.texCoords.push(0.5, 0.5);
+		this.vertices.push(0,0,1); // Center
+		this.normals.push(0,0,1); // Center
+		this.texCoords.push(0.5, 0.5);
+		
+		console.log(this.indices);
         
-        // //Lower-half
+		//Lower-half
+		
+		var indicesLength = (this.vertices.length)/3;
 
-        // for (var j = 0; j < this.stacks; j++)
-		// {
-		// 	for (var i = 0; i < this.slices; i++)
-		// 	{
+        for (var j = 0; j < this.stacks; j++)
+		{
+			for (var i = 0; i < this.slices; i++)
+			{
+				this.vertices.push(Math.cos(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)), Math.sin(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)), -j/this.stacks);				// A
 
-		// 		this.vertices.push(Math.cos(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)), Math.sin(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)), -j/this.stacks);				// A
+				if (j == this.stacks - 1)
+				{
+					this.indices.push(indicesLength + foo((0 + i)%(this.slices) + j*this.slices, this.slices*this.stacks)); this.indices.push(indicesLength + foo((1 + i)%(this.slices) + (j+1)*this.slices, this.slices*this.stacks)); this.indices.push(indicesLength + foo((1 + i)%(this.slices) + j*this.slices, this.slices*this.stacks)); // ACB
+				}
+				else
+				{
+					this.indices.push(indicesLength + foo((0 + i)%(this.slices) + j*this.slices, this.slices*this.stacks)); this.indices.push(indicesLength + foo((1 + i)%(this.slices) + (j+1)*this.slices, this.slices*this.stacks)); this.indices.push(indicesLength + foo((1 + i)%(this.slices) + j*this.slices, this.slices*this.stacks));// ACB
+					this.indices.push(indicesLength + foo((0 + i)%(this.slices) + j*this.slices, this.slices*this.stacks)); this.indices.push(indicesLength + foo((0 + i)%(this.slices) + (j+1)*this.slices, this.slices*this.stacks)); this.indices.push(indicesLength + foo((1 + i)%(this.slices) + (j+1)*this.slices, this.slices*this.stacks));// BCD
+				}
 
-		// 		if (j == this.stacks -1)
-		// 		{
-		// 			this.indices.push(foo((0 + i)%(this.slices) + j*this.slices, this.slices*this.stacks)); this.indices.push(foo((1 + i)%(this.slices) + j*this.slices, this.slices*this.stacks)); this.indices.push(foo((1 + i)%(this.slices) + (j+1)*this.slices, this.slices*this.stacks)); // ACB
-		// 		}
-		// 		else
-		// 		{
-		// 			this.indices.push(foo((0 + i)%(this.slices) + j*this.slices, this.slices*this.stacks)); this.indices.push(foo((1 + i)%(this.slices) + j*this.slices, this.slices*this.stacks)); this.indices.push(foo((1 + i)%(this.slices) + (j+1)*this.slices, this.slices*this.stacks)); // ACB
-		// 			this.indices.push(foo((0 + i)%(this.slices) + j*this.slices, this.slices*this.stacks)); this.indices.push(foo((1 + i)%(this.slices) + (j+1)*this.slices, this.slices*this.stacks)); this.indices.push(foo((0 + i)%(this.slices) + (j+1)*this.slices, this.slices*this.stacks)); // BCD
-		// 		}
+				this.normals.push(Math.cos(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)), Math.sin(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)), -j/this.stacks);	// Vector from center (0,0,0) to P(x,y,z) is (x,y,z), therefore equal to the point it is applied to.
+				this.texCoords.push((Math.cos(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)) + 1)/2, (-Math.sin(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)) + 1)/2);
 
-		// 		this.normals.push(Math.cos(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)), Math.sin(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)), -j/this.stacks);	// Vector from center (0,0,0) to P(x,y,z) is (x,y,z), therefore equal to the point it is applied to.
-		// 		this.texCoords.push((Math.cos(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)) + 1)/2, (-Math.sin(alpha*i)*Math.sqrt(1 - Math.pow(j/this.stacks,2)) + 1)/2);
-
-        //     }
+            }
             
-		// }
+		}
 
-		// this.vertices.push(0,0,-1); // Center
-		// this.normals.push(0,0,-1); // Center
-		// this.texCoords.push(0.5, 0.5);
+		this.vertices.push(0,0,-1); // Center
+		this.normals.push(0,0,-1); // Center
+		this.texCoords.push(0.5, 0.5);
+		
+		console.log(this.indices);
         
         this.primitiveType=this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
