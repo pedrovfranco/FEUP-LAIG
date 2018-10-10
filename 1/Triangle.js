481d0b4eb2c910618edab21585bd0192f5107377
pedrofixe <yurbypedro@hotmai.com>
2018-10-10 09:35:29 +0100

    Finished triangle normals

--- 1/Triangle.js
+++ 1/Triangle.js
@@ -1,11 +1,78 @@
+function swap(a, b)
+{
+    var temp = a;
+    a = b;
+    b = temp;
+}
+
+function solve_linear(matrix) {
+	var temp;
+	if (matrix[0][0] == 0)
+		for(var i = 0; i < matrix.length; i++)
+			if (matrix[i][0] != 0)
+				swap(matrix[0], matrix[i]);
+
+	for(var i = 0; i < matrix.length - 1; i++)
+	{
+		for(var j = i + 1; j < matrix.length; j++)
+		{
+			temp = matrix[j][i] / matrix[i][i];
+
+			for(var k = i; k < matrix[i].length; k++)
+				matrix[j][k] -= matrix[i][k]*temp;
+
+		}
+
+	}
+
+	for(var i = 0; i < matrix.length; i++)
+	{
+		temp = matrix[i][i];
+
+		for(var k = 0; k < matrix[i].length; k++)
+			matrix[i][k] /= temp;
+
+	}
+
+	var res = Array.from(matrix[0].keys());
+	temp = 0;
+	for(var i = matrix.length - 1; i >= 0; i--)
+	{
+		temp = matrix[i][matrix[i].length-1];
+		for(var j = i + 1; j < matrix[i].length - 1; j++)
+		{
+			temp -= matrix[i][j] * res[j];
+		}
+		res[i] = temp;
+	}
+
+	return res;
+}
+
+function normalizeVector(n)
+{
+    var size = 0;
+
+    for (var i = 0; i < n.length; i++)
+    {
+        size += n[i]*n[i];
+    }
+
+    size = Math.sqrt(size);
+
+    for (var i = 0; i < n.length; i++)
+    {
+        n[i] /= size;
+    }
+
+    return n;
+}
+
 function normal(x1,y1,z1,x2,y2,z2,x3,y3,z3)
 {
     var u = [x2-x1, y2-y1, z2-z1], v = [x3-x2, y3-y2, z3-z2];
 
-    var n = [1,0,0];
-
-    n[1] = ((v[2]*u[0]/u[2] - v[0])/v[1])/(1 - u[1]*v[2]/u[2]/v[1]);
-    n[2] = (-u[0] - u[1]*n[1])/u[2];
+    var n = normalizeVector(solve_linear([u,v]));
 
     return n;
 }
@@ -45,7 +112,6 @@ class Triangle extends Primitive
             
         var foo = normal(this.x1, this.y1, this.z1, this.x2, this.y2, this.z2, this.x3, this.y3, this.z3);
         this.normals = [
-            foo[0], foo[1], foo[2],
             foo[0], foo[1], foo[2],
             foo[0], foo[1], foo[2],
             foo[0], foo[1], foo[2]
