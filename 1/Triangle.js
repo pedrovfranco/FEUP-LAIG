b1f74bac1d33b7ca39a10f713d775133952c6020
pedrofixe <yurbypedro@hotmai.com>
2018-10-10 11:59:36 +0100

    Fixed triangle normals

--- 1/Triangle.js
+++ 1/Triangle.js
@@ -1,78 +1,14 @@
-function swap(a, b)
-{
-    var temp = a;
-    a = b;
-    b = temp;
-}
-
-function solve_linear(matrix) {
-	var temp;
-	if (matrix[0][0] == 0)
-		for(var i = 0; i < matrix.length; i++)
-			if (matrix[i][0] != 0)
-				swap(matrix[0], matrix[i]);
-
-	for(var i = 0; i < matrix.length - 1; i++)
-	{
-		for(var j = i + 1; j < matrix.length; j++)
-		{
-			temp = matrix[j][i] / matrix[i][i];
-
-			for(var k = i; k < matrix[i].length; k++)
-				matrix[j][k] -= matrix[i][k]*temp;
-
-		}
-
-	}
-
-	for(var i = 0; i < matrix.length; i++)
-	{
-		temp = matrix[i][i];
-
-		for(var k = 0; k < matrix[i].length; k++)
-			matrix[i][k] /= temp;
-
-	}
-
-	var res = Array.from(matrix[0].keys());
-	temp = 0;
-	for(var i = matrix.length - 1; i >= 0; i--)
-	{
-		temp = matrix[i][matrix[i].length-1];
-		for(var j = i + 1; j < matrix[i].length - 1; j++)
-		{
-			temp -= matrix[i][j] * res[j];
-		}
-		res[i] = temp;
-	}
-
-	return res;
-}
-
-function normalizeVector(n)
-{
-    var size = 0;
-
-    for (var i = 0; i < n.length; i++)
-    {
-        size += n[i]*n[i];
-    }
-
-    size = Math.sqrt(size);
-
-    for (var i = 0; i < n.length; i++)
-    {
-        n[i] /= size;
-    }
-
-    return n;
-}
-
 function normal(x1,y1,z1,x2,y2,z2,x3,y3,z3)
 {
     var u = [x2-x1, y2-y1, z2-z1], v = [x3-x2, y3-y2, z3-z2];
 
-    var n = normalizeVector(solve_linear([u,v]));
+	var n = [
+		u[1]*v[2] - u[2]*u[1],
+		u[2]*v[0] - u[0]*v[2],
+		u[0]*v[1] - u[1]*v[0]
+	];
+
+    var n = normalizeVector(n);
 
     return n;
 }
